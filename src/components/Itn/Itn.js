531cd295be66ca92a05c0309ddc7eb2f8d8f6e5a
Volodya <volodyaserobyan732@gmail.com>
2020-02-04 23:04:59 -0800

    Complete Pool Stat Refactoring Meri Part 2

--- src/components/Itn/Itn.js
+++ src/components/Itn/Itn.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardStat from './CardStat'
+import PoolStat from './PoolStat'
 import './Itn.scss'
 
 const Itn = () => {
@@ -8,7 +9,10 @@ const Itn = () => {
         <section className="Itn">
             <div className="Itn-Cont">
                 <div className="Itn-Cont-Card">
-                    {[0,1,2].map(id => <CardStat key={id} id={id} />)}
+                    {[0, 1, 2].map(id => <CardStat key={id} id={id} />)}
+                </div>
+                <div className="Itn-Cont-Pool">
+                    {[0, 1].map(id => <PoolStat key={id} id={id} />)}
                 </div>
             </div>
         </section>
--- src/components/Itn/PoolStat/PoolStat.js
+++ src/components/Itn/PoolStat/PoolStat.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import earth from '../../../assets/earth.png'
+import money from '../../../assets/money.png'
 import './PoolStat.scss'
 
 const PoolStat = () => {
 
     return (
         <section className="PoolStat">
-
+            <div className="PoolStat-Cont">
+                <h1>Pool Stat</h1>
+                <div className="PoolStat-Cont-Context">
+                    <div>
+                        <img src={money} alt='' />
+                        <div>
+                            {/* <p>Live stake (ADA)</p> */}
+                            <h1>49,550,815</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="PoolStat-Cont-Footer">
+                    <div>
+                        <h1>1,193</h1>
+                        <p>Total blocks generated</p>
+                    </div>
+                    <div>
+                        <h1>17,848</h1>
+                        <p>Last epoch rewards (ADA)</p>
+                    </div>
+                    <div>
+                        <h1>5 hours ago</h1>
+                        <p>Last block generated</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
