c38958c6e9f53b9a75ca82f8199cdf7a3c337397
Volodya <volodyaserobyan732@gmail.com>
2020-02-10 05:01:28 -0800

    merge

---
+++ build/precache-manifest.6c5e4c7aa58a6f07dbd82f6dcd228e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c46e05fc0cc7789471550b007d03c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d07345dc4458f862256",
+    "url": "/static/css/main.66f5126a.chunk.css"
+  },
+  {
+    "revision": "231318c28b0e6d2ea50c",
+    "url": "/static/js/2.b41284b1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b41284b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d07345dc4458f862256",
+    "url": "/static/js/main.ef15c4d4.chunk.js"
+  },
+  {
+    "revision": "9056642bd6f194717cc6",
+    "url": "/static/js/runtime-main.4efa1967.js"
+  }
+]);
